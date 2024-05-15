28bab7dce6e61392d5804ad07565145d55de64b6
Mlkpanpas1 <168426868+Mlkpanpas1@users.noreply.github.com>
2024-05-15 11:27:41 -0300

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,24 @@
+package calculadora;
+
+
+public class Calculadora {
+
+
+	private int a;
+	private int b;
+  
+   public int soma () {
+       return a + b;
+   }
+ 
+   public int subtrair () {
+       return a - b;
+   }
+   public int multiplicar () {
+       return a * b;
+   }
+ 
+   public double dividir () {
+       return a / b;
+   }
+}
